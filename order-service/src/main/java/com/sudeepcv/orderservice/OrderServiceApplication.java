1640f2d23b25a83c719f868570e3ded181af6fb6
sudeepcv <sudeepcv@sudeepcvs-MacBook-Air.local>
2023-02-26 23:49:58 +0530

    on start queue created

--- order-service/src/main/java/com/sudeepcv/orderservice/OrderServiceApplication.java
+++ order-service/src/main/java/com/sudeepcv/orderservice/OrderServiceApplication.java
@@ -1,13 +1,35 @@
 package com.sudeepcv.orderservice;
 
+import jakarta.annotation.PostConstruct;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.core.RabbitAdmin;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class OrderServiceApplication {
+	@Autowired
+	private RabbitAdmin rabbitAdmin;
+	@Value("${rabbitmq.queue.order.name}")
+	private String orderQueueName;
+
+	@Value("${rabbitmq.queue.email.name}")
+	private String emailQueue;
 
 	public static void main(String[] args) {
 		SpringApplication.run(OrderServiceApplication.class, args);
+	}
+
+	@PostConstruct
+	public void createQueues() {
+		System.out.println("queue created...........................................................");
+		Queue queue1 = new Queue(orderQueueName);
+		rabbitAdmin.declareQueue(queue1);
+
+		Queue queue2 = new Queue(emailQueue);
+		rabbitAdmin.declareQueue(queue2);
 	}
 
 }
--- order-service/src/main/java/com/sudeepcv/orderservice/config/RabbitMQConfig.java
+++ order-service/src/main/java/com/sudeepcv/orderservice/config/RabbitMQConfig.java
@@ -1,7 +1,9 @@
 package com.sudeepcv.orderservice.config;
 
 import org.springframework.amqp.core.*;
+import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitAdmin;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
@@ -24,6 +26,8 @@ public class RabbitMQConfig {
 
     @Value("${rabbitmq.queue.email.name}")
     private String emailQueue;
+    @Value("${spring.rabbitmq.host}")
+    private String host;
 
     @Bean
     public Queue orderQueue(){
@@ -64,5 +68,15 @@ public class RabbitMQConfig {
         rabbitTemplate.setMessageConverter(converter());
         return rabbitTemplate;
     }
+    @Bean
+    public CachingConnectionFactory connectionFactory() {
+        return new CachingConnectionFactory(host);
+    }
+    @Bean
+    public RabbitAdmin amqpAdmin() {
+        return new RabbitAdmin(connectionFactory());
+    }
+
+
 }
 
